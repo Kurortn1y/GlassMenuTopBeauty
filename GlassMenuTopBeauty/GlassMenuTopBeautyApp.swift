553583202eb8deb13060d0d56b7b5cfb629204e6
Roman Kondrashov <lastmandanc1ng@icloud.com>
2025-10-07 19:46:17 +0300

    Initial Commit

---
+++ GlassMenuTopBeauty/GlassMenuTopBeautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassMenuTopBeautyApp.swift
+//  GlassMenuTopBeauty
+//
+//  Created by Roman Kondrashov on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct GlassMenuTopBeautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
